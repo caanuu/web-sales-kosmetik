ead729279bc515e1f7f8b9f2c07c974312564e32
caanuu <ihsanarifab12@gmail.com>
2026-05-02 12:51:49 +0700

    update

--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -83,7 +83,7 @@
                 <input type="hidden" name="product_id" value="{{ $product->id }}">
                 <button type="submit" class="w-9 h-9 btn-primary rounded-full flex items-center justify-center text-white" title="Tambah ke Keranjang"
                     {{ $product->stock < 1 ? 'disabled' : '' }}>
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/></svg>
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                 </button>
             </form>
             @endauth
